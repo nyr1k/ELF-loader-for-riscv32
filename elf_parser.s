313cd77b6493a160bd91da9f80374ee8c7640995
nyr1k <zolamanuraly@gmail.com>
2026-07-01 01:06:07 +0500

    reads the elf_header, and checks if the file is indeed a elf file for 32-bit riscv system. returns 1 on error (check with <echo 2>)

--- elf_parser.s
+++ elf_parser.s
@@ -1,12 +1,12 @@
 section .data
   testMsg db "Hello World", 10 
   newline db 10 
+  elf_header db 52 ; elf header struct for 32-bit elfs 
 
 section .text
   global _start 
 
 _start:
-
   ; if argc != 2 -> exit 
   cmp qword [rsp], 2
   jne exit_error 
@@ -17,28 +17,32 @@ _start:
   mov rsi, 0 ; O_RDONLY
   mov rdx, 0 ; ignore mode
   syscall 
-
   ; if return value < 0 -> exit 
   cmp rax, 0 
   jl exit_error 
-
-  push rax ; save the file descriptor
+  ; save the file descriptor
+  push rax 
   
-  sub rsp, 1024 ; allocate read buffer 
+  ; load the ELF header 
+  mov rax, 0 ; sys_read
+  mov rdi, [rsp] ; fd
+  mov rsi, elf_header ; buffer
+  mov rdx, 52 ; bytes to read
+  syscall    
+  ; if return value < 0 -> exit 
+  cmp rax, 0
+  jl exit_error
 
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, testMsg
-  mov rdx, 12
-  syscall  
+  mov rdi, elf_header 
+  call check_header
+  
 
-  add rsp, 1024 ; deallocate read buffer
-  pop rdi ; pop the file descriptor
-
+.exit: 
+  ; get the file descriptor
+  pop rdi  
   ; close the ELF file
   mov rax, 3 ; sys_close
   syscall 
-
   ; if return value < 0 -> exit
   cmp rax, 0
   jl exit_error
@@ -47,6 +51,22 @@ _start:
   mov rax, 60 
   xor edi, edi 
   syscall
+
+;;;
+check_header:
+  ; check magic numbers
+  cmp dword[rdi], 0x464c457f ; this hex is '.ELF' in little-endian mode
+  jne exit_error 
+  
+  ; check bitness and endian mode 
+  cmp word[rdi+4], 0x0101 ; 32-bit and little-endian 
+  jne exit_error  
+
+  ; file type (must be executalbe) and architecture (must be RISC-V) 
+  cmp dword[rdi+16], 0x00F30002 ; 0x02 = Executable file
+  jne exit_error
+
+  ret
 
 ;;;
 strlen:
